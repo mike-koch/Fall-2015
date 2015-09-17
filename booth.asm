ad8427f673bcb1b2d978a5e763df29a7aeab6624
Mike Koch <mkoch227@users.noreply.github.com>
2015-09-17 00:02:15 -0400

    Finished a basic writeup
    Everything is compiling, but not sure if it's even working

--- booth.asm
+++ booth.asm
@@ -11,17 +11,26 @@ Booth	PROC
 	mov dx, 0                ; 4. Initialize DX to zero
 	clc                      ; 5. Clear the carry flag
 	; STEP 6: Check LSB of Multiplier and CF
-	mov di, ax
-	and di, 0001h            ; We're checking the least significant bit to see if it is 1 by ANDing the multiplier's LSB to with 1
-	jz  LSB_0                ; Jump if the least significant bit was 0.
-	; Since we didn't jump, LSB = 1
-	jc  BOTH_EQUAL           ; If the carry flag is set, then both the LSB and the carry flag are = 1
-	; Since we didn't jump, LSB = 1, CF = 0. Simply continue below
-	;; TODO Step 8
-	BOTH_EQUAL:
-	;; TODO Step 10
-	LSB_0:
-		jnc BOTH_EQUAL       ; Jump if the least significant bit is 0 and the carry flag isn't set
-		;; TODO Step 9
+	PROCEDURE:
+		mov di, ax
+		and di, 0001h            ; We're checking the least significant bit to see if it is 1 by ANDing the multiplier's LSB to with 1
+		jz  LSB_0                ; Jump if the least significant bit was 0.
+		; Since we didn't jump, LSB = 1
+		jc  BOTH_EQUAL           ; If the carry flag is set, then both the LSB and the carry flag are = 1
+		; Since we didn't jump, LSB = 1, CF = 0. Simply continue below
+		sub bx, dx               ; Subtract the multiplicand from the high word of DX:AX
+		BOTH_EQUAL:
+			sar dx, 1            ; Perform a 'shift arithmetic right' 1 to shift high word into DX into the low word of partial product DX:AX
+			rcr ax, 1            ; Rotate through carry the low bit 1 place to make it the new carry flag value
+			dec cx               ; Decrement the loop counter
+			jcxz FINISH          ; If cx is 0, let's exit the program
+			jmp PROCEDURE        ; Otherwise we'll perform another iteration
+		LSB_0:
+			jnc BOTH_EQUAL       ; Jump if the least significant bit is 0 and the carry flag isn't set
+			add bx, dx           ; Add the multiplicand from the high word of DX:AX
+			jmp BOTH_EQUAL
+	FINISH:
+		mov ax, 4c00h
+		int 21h
 Booth	ENDP
-	END	Hello
+	END	Booth
