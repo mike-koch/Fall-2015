f6b3f5e56b750cd168853fb90081bdd1c9bf87dc
Mike Koch <mkoch227@gmail.com>
2015-09-29 13:33:52 -0400

    Add skeleton for binary calculator

---
+++ Transmitter/physical/BinaryCalculator.cpp
@@ -0,0 +1,9 @@
+//
+// Created by mkoch on 9/29/15.
+//
+
+#include "BinaryCalculator.h"
+const char* convert_char_to_binary(char &character) {
+    // TODO Complete this
+    return "";
+}
---
+++ Transmitter/physical/BinaryCalculator.h
@@ -0,0 +1,8 @@
+//
+// Created by mkoch on 9/29/15.
+//
+
+#ifndef TRANSMITTER_BINARYCALCULATOR_H
+#define TRANSMITTER_BINARYCALCULATOR_H
+const char* convert_char_to_binary(char &character);
+#endif //TRANSMITTER_BINARYCALCULATOR_H
