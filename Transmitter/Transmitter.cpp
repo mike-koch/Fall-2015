c3a33638826eed927e80c2e002a6bd97f64d0b7b
Mike Koch <mkoch227@gmail.com>
2015-09-29 11:08:12 -0400

    Improved clarity of sample code

--- Transmitter/Transmitter.cpp
+++ Transmitter/Transmitter.cpp
@@ -5,8 +5,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <iostream>
 
 void error(char* msg);
+void check_args(int argc);
+void setupSocket(char *const *argv, int sockfd, int newsockfd, int portno, int &clilen, sockaddr_in &serv_addr, sockaddr_in &cli_addr);
+
+int setupSocket(char *const *argv, int sockfd, int portno, int &clilen, sockaddr_in &serv_addr, sockaddr_in &cli_addr);
 
 int main(int argc, char *argv[])
 {
@@ -15,11 +20,27 @@ int main(int argc, char *argv[])
     struct sockaddr_in serv_addr, cli_addr;
     int n;
 
-    if( argc < 2 )
-    {
-        fprintf(stderr,"ERROR, no port provided\n");
-        exit(1);
-    }
+    check_args(argc);
+
+    setupSocket(argv, sockfd, newsockfd, portno, clilen, serv_addr, cli_addr);
+
+    // At this point, setupSocket will be waiting for the client to connect, so nothing will execute below here until
+    //     the client has connected.  Once the client connects, we can start transmitting.
+
+    if( newsockfd < 0 )
+        error("ERROR on accept");
+    bzero(buffer,256);
+    n = read(newsockfd,buffer,255);
+    if( n < 0 )
+        error("ERROR reading from socket");
+    printf("Here is the message: %s\n",buffer);
+    n = write(newsockfd,"I got your message",18);
+    if( n < 0 )
+        error("ERROR writing to socket");
+    return 0;
+}
+
+void setupSocket(char *const *argv, int sockfd, int newsockfd, int portno, int &clilen, sockaddr_in &serv_addr, sockaddr_in &cli_addr) {
     fprintf(stdout, "Run client by providing host and port\n");
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
@@ -37,21 +58,17 @@ int main(int argc, char *argv[])
     newsockfd = accept(sockfd,
                        (struct sockaddr *) &cli_addr,
                        (socklen_t *) &clilen);
-    if( newsockfd < 0 )
-        error("ERROR on accept");
-    bzero(buffer,256);
-    n = read(newsockfd,buffer,255);
-    if( n < 0 )
-        error("ERROR reading from socket");
-    printf("Here is the message: %s\n",buffer);
-    n = write(newsockfd,"I got your message",18);
-    if( n < 0 )
-        error("ERROR writing to socket");
-    return 0;
 }
 
-void error( char *msg )
-{
+void error( char *msg ) {
     perror(msg);
     exit(1);
+}
+
+void check_args(int argc) {
+    if( argc < 2 )
+    {
+        fprintf(stderr,"ERROR, no port provided\n");
+        exit(1);
+    }
 }
