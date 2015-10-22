3b8e3815ee2ff925f2a9b4ed17b81a7396c4cd6c
Mike Koch <mkoch227@gmail.com>
2015-10-22 18:18:03 -0400

    Add skeleton for CRC

---
+++ Transmitter/physical/CRC.cpp
@@ -0,0 +1,9 @@
+//
+// Created by mkoch on 10/22/15.
+//
+
+#include "CRC.h"
+char* get_crc_16(char *message_to_transmit, int length_of_message) {
+    // TODO
+    return message_to_transmit;
+}
---
+++ Transmitter/physical/CRC.h
@@ -0,0 +1,10 @@
+//
+// Created by mkoch on 10/22/15.
+//
+
+#ifndef TRANSMITTER_CRCCHECK_H
+#define TRANSMITTER_CRCCHECK_H
+
+char* get_crc_16(char *message_to_transmit, int length_of_message);
+
+#endif //TRANSMITTER_CRCCHECK_H
