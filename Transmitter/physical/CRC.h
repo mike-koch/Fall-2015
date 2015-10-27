ceeb0f47e8d5abf80461d23460f355ae7c70b11c
Mike Koch <mkoch227@gmail.com>
2015-10-27 15:59:58 -0400

    Add CRC polynomial

--- Transmitter/physical/CRC.h
+++ Transmitter/physical/CRC.h
@@ -5,6 +5,9 @@
 #ifndef TRANSMITTER_CRCCHECK_H
 #define TRANSMITTER_CRCCHECK_H
 
+// CRC-ANSI: x^16 + x^15 + x^2 + 1
+#define CRC_POLY 0x8005
+
 char* get_crc_16(char *message_to_transmit, int length_of_message);
 
 #endif //TRANSMITTER_CRCCHECK_H
