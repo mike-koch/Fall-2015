7b481c87133bce4076c7a49b9e56c4d58a1fbf21
Mike Koch <mkoch227@gmail.com>
2015-10-29 12:01:54 -0400

    Add CRC implementation

--- Transmitter/physical/CRC.cpp
+++ Transmitter/physical/CRC.cpp
@@ -1,5 +1,24 @@
 #include "CRC.h"
-char* get_crc_16(char *message_to_transmit, int length_of_message) {
-    // TODO
-    return message_to_transmit;
+
+// CRC-ANSI: x^16 + x^15 + x^2 + 1
+#define CRC_POLY 0x8005
+
+
+uint16_t get_crc_16(char *message_to_transmit, int length_of_message) {
+    uint16_t crc = 0;
+    uint16_t poly = CRC_POLY;
+
+    for (int dataByte = 0; dataByte < length_of_message; dataByte++) {
+        crc ^= message_to_transmit[dataByte] << 8;
+
+        for (int i = 0; i < 8; i++) {
+            int top_bit = crc & (1 << 15);
+            crc <<= 1;
+            if (top_bit) {
+                crc ^= poly;
+            }
+        }
+    }
+
+    return crc;
 }
--- Transmitter/physical/CRC.h
+++ Transmitter/physical/CRC.h
@@ -1,9 +1,9 @@
 #ifndef TRANSMITTER_CRC_H
 #define TRANSMITTER_CRC_H
 
-// CRC-ANSI: x^16 + x^15 + x^2 + 1
-#define CRC_POLY 0x8005
 
-char* get_crc_16(char *message_to_transmit, int length_of_message);
+#include <stdint.h>
+
+uint16_t get_crc_16(char *message_to_transmit, int length_of_message);
 
 #endif //TRANSMITTER_CRC_H
