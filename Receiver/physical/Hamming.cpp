1cd842018bdb58d21917817dcc4ef78afe8c2eea
Mike Koch <mkoch227@gmail.com>
2015-11-11 20:04:46 -0500

    Hamming receiver is working

--- Receiver/physical/Hamming.cpp
+++ Receiver/physical/Hamming.cpp
@@ -36,14 +36,14 @@ void reverse_hamming(char *message, int length, char *returned_data) {
 
         // Build the byte now that we have checked hamming.
         char next_byte[8];
-        next_byte[0] = message[i + 2];
-        next_byte[1] = message[i + 4];
-        next_byte[2] = message[i + 5];
-        next_byte[3] = message[i + 6];
-        next_byte[4] = message[i + 8];
-        next_byte[5] = message[i + 9];
-        next_byte[6] = message[i + 10];
-        next_byte[7] = message[i + 11];
+        next_byte[0] = message[i + 9];
+        next_byte[1] = message[i + 7];
+        next_byte[2] = message[i + 6];
+        next_byte[3] = message[i + 5];
+        next_byte[4] = message[i + 3];
+        next_byte[5] = message[i + 2];
+        next_byte[6] = message[i + 1];
+        next_byte[7] = message[i];
 
         returned_data[returned_data_index] = get_parsed_character(next_byte);
     }
@@ -57,45 +57,45 @@ int check_hamming(char *message, int starting_index) {
     int syndrome = 0;
     int value = 1;
     value ^= get_int_value_of_char(message[11 - starting_index]);
-    value ^= get_int_value_of_char(message[11 - starting_index + 2]);
-    value ^= get_int_value_of_char(message[11 - starting_index + 4]);
-    value ^= get_int_value_of_char(message[11 - starting_index + 6]);
-    value ^= get_int_value_of_char(message[11 - starting_index + 8]);
-    value ^= get_int_value_of_char(message[11 - starting_index + 10]);
+    value ^= get_int_value_of_char(message[11 - (starting_index + 2)]);
+    value ^= get_int_value_of_char(message[11 - (starting_index + 4)]);
+    value ^= get_int_value_of_char(message[11 - (starting_index + 6)]);
+    value ^= get_int_value_of_char(message[11 - (starting_index + 8)]);
+    value ^= get_int_value_of_char(message[11 - (starting_index + 10)]);
     if (value != 0) {
         syndrome += 1;
     }
 
     // Parity bit #2 (position 2) calculates values at 2, 3, 6, 7, 10, 11. A non-zero value adds 2 to the syndrome.
     value = 1;
-    value ^= get_int_value_of_char(message[11 - starting_index + 1]);
-    value ^= get_int_value_of_char(message[11 - starting_index + 2]);
-    value ^= get_int_value_of_char(message[11 - starting_index + 5]);
-    value ^= get_int_value_of_char(message[11 - starting_index + 6]);
-    value ^= get_int_value_of_char(message[11 - starting_index + 9]);
-    value ^= get_int_value_of_char(message[11 - starting_index + 10]);
+    value ^= get_int_value_of_char(message[11 - (starting_index + 1)]);
+    value ^= get_int_value_of_char(message[11 - (starting_index + 2)]);
+    value ^= get_int_value_of_char(message[11 - (starting_index + 5)]);
+    value ^= get_int_value_of_char(message[11 - (starting_index + 6)]);
+    value ^= get_int_value_of_char(message[11 - (starting_index + 9)]);
+    value ^= get_int_value_of_char(message[11 - (starting_index + 10)]);
     if (value != 0) {
         syndrome += 2;
     }
 
     // Parity bit #3 (position 4) calculates values at 4, 5, 6, 7, 12. A non-zero value returns 4 to the syndrome.
     value = 1;
-    value ^= get_int_value_of_char(message[11 - starting_index + 3]);
-    value ^= get_int_value_of_char(message[11 - starting_index + 4]);
-    value ^= get_int_value_of_char(message[11 - starting_index + 5]);
-    value ^= get_int_value_of_char(message[11 - starting_index + 6]);
-    value ^= get_int_value_of_char(message[11 - starting_index + 11]);
+    value ^= get_int_value_of_char(message[11 - (starting_index + 3)]);
+    value ^= get_int_value_of_char(message[11 - (starting_index + 4)]);
+    value ^= get_int_value_of_char(message[11 - (starting_index + 5)]);
+    value ^= get_int_value_of_char(message[11 - (starting_index + 6)]);
+    value ^= get_int_value_of_char(message[11 - (starting_index + 11)]);
     if (value != 0) {
         syndrome += 4;
     }
 
     // Parity bit #4 (position 8) calculates values at 8, 9, 10, 11, 12. A non-zero value returns 8 to the syndrome.
     value = 1;
-    value ^= get_int_value_of_char(message[11 - starting_index + 7]);
-    value ^= get_int_value_of_char(message[11 - starting_index + 8]);
-    value ^= get_int_value_of_char(message[11 - starting_index + 9]);
-    value ^= get_int_value_of_char(message[11 - starting_index + 10]);
-    value ^= get_int_value_of_char(message[11 - starting_index + 11]);
+    value ^= get_int_value_of_char(message[11 - (starting_index + 7)]);
+    value ^= get_int_value_of_char(message[11 - (starting_index + 8)]);
+    value ^= get_int_value_of_char(message[11 - (starting_index + 9)]);
+    value ^= get_int_value_of_char(message[11 - (starting_index + 10)]);
+    value ^= get_int_value_of_char(message[11 - (starting_index + 11)]);
     if (value != 0) {
         syndrome += 8;
     }
