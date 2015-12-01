289d92c1b6137e1ca4a84b907cf0ebb84f9877d1
Mike Koch <mkoch227@gmail.com>
2015-12-01 14:32:59 -0500

    More changes

--- Receiver/Receiver.cpp
+++ Receiver/Receiver.cpp
@@ -4,21 +4,25 @@
 #include <string.h>
 #include <stdlib.h>
 #include "physical/ReceivingUtils.h"
+#include "enum/ErrorCodes.h"
 
-void check_args(int argc);
 void setup_socket(char *const *argv, int &newsockfd);
 void error(const char *msg) {
     perror(msg);
     exit(0);
 }
 bool socket_is_alive(int sockfd);
+void process_args(int argc, char *argv[]);
+void print_help();
+
+char *port_number;
 
 int main(int argc, char *argv[])
 {
     int newsockfd;
 
-    check_args(argc);
-    setup_socket(argv, newsockfd);
+    process_args(argc, argv);
+    setup_socket(newsockfd);
 
     // At this point, setup_socket will be waiting for the client to connect, so nothing will execute below here until
     //     the client has connected.  Once the client connects, we can start transmitting.
@@ -48,7 +52,7 @@ void check_args(int argc) {
     }
 }
 
-void setup_socket(char *const *argv, int &newsockfd) {
+void setup_socket(int &newsockfd) {
     struct sockaddr_in serv_addr, cli_addr;
 
     fprintf(stderr, "Run client by providing host and port\n----------------------------------------\n");
@@ -59,7 +63,7 @@ void setup_socket(char *const *argv, int &newsockfd) {
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons((uint16_t) atoi(argv[1]));
+    serv_addr.sin_port = htons((uint16_t) atoi(port_number));
     if (bind(sockfd, (struct sockaddr *) &serv_addr,
              sizeof(serv_addr)) < 0) {
         error("ERROR on binding");
@@ -72,4 +76,34 @@ void setup_socket(char *const *argv, int &newsockfd) {
     if( newsockfd < 0 ) {
         error("ERROR on accept");
     }
+}
+
+void process_args(int argc, char *argv[]) {
+    const char *args = "p:h";
+    int c;
+
+    while ((c = getopt(argc, argv, args)) != -1) {
+        switch (c) {
+            case 'p':
+                port_number = optarg;
+                break;
+            case '?':
+                std::cerr << "Unknown argument: " << optopt << std::endl;
+                print_help();
+                exit(ERROR_UNKNOWN_PARAMETER);
+            case 'h':
+            default:
+                print_help();
+                exit(0);
+        }
+    }
+}
+
+void print_help() {
+    std::cout << "Usage: Receiver [-p <port number>] [-a | -c]";
+    std::cout << "-p: Port number for the receiver to listen to.\n";
+    std::cout << "-a: Error correction using Hamming encoding. This must be consistent with the transmitter's error detection/correction mode\n";
+    std::cout << "-c: Error detection using CRC. This must be consistent with the receiver's error detection/correction mode\n";
+    std::cout << "*** Only -a or -c can be chosen, NOT both. ***\n";
+    std::cout << "-h: Display this help text";
 }
--- Transmitter/Transmitter.cpp
+++ Transmitter/Transmitter.cpp
@@ -9,27 +9,37 @@
 #include "app/FileManager.h"
 #include "datalink/Framing.h"
 #include "physical/TransmissionUtils.h"
+#include "enum/ErrorCodes.h"
 
 using namespace std;
 
 void error(const char* msg);
-void check_args(int argc);
-void connect_to_server(char *argv[], int &sockfd);
+void process_args(int argc, char *argv[]);
+void connect_to_server(int &sockfd);
+void print_help();
+
+char *file_name;
+char *port_number;
+char *host_name;
 
 int main(int argc, char *argv[])
 {
     int sockfd;
 
-    check_args(argc);
+    process_args(argc, argv);
 
-    connect_to_server(argv, sockfd);
+    SendMode sendMode = SendMode::SOCKET;
+
+    if (sendMode == SendMode::SOCKET) {
+        connect_to_server(sockfd);
+    }
 
     std::string message =
             retrieve_file_to_transmit(argv[3]);
     for (unsigned int i = 0; i < strlen(message.c_str()); i += 64) {
         Frame *frame = new Frame();
         build_frame(message.c_str(), i, frame);
-        send(frame, SendMode::SOCKET, sockfd);
+        send(frame, sendMode, sockfd);
         delete frame;
     }
 
@@ -38,13 +48,13 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-void connect_to_server(char *argv[], int &sockfd) {
+void connect_to_server(int &sockfd) {
     struct sockaddr_in serv_addr;
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if( sockfd < 0 )
         error("ERROR opening socket");
-    hostent *server = gethostbyname(argv[1]);
+    hostent *server = gethostbyname(host_name);
     if( server == NULL )
     {
         fprintf(stderr,"ERROR, no such host\n");
@@ -55,7 +65,7 @@ void connect_to_server(char *argv[], int &sockfd) {
     bcopy(server->h_addr,
           (char *)&serv_addr.sin_addr.s_addr,
           server->h_length);
-    serv_addr.sin_port = htons(atoi(argv[2]));
+    serv_addr.sin_port = htons(atoi(port_number));
     if( connect(sockfd, (const sockaddr *) &serv_addr,sizeof(serv_addr)) < 0 )
         error("ERROR connecting");
 }
@@ -65,9 +75,37 @@ void error(const char *msg) {
     exit(1);
 }
 
-void check_args(int argc) {
-    if (argc < 4) {
-        std::cerr << "Usage: Transmitter.cpp [hostname] [port] [filename]";
-        exit(0);
+void process_args(int argc, char *argv[]) {
+    const char *args = "i:n:p:h";
+    int c;
+
+    while ((c = getopt(argc, argv, args)) != -1) {
+        switch (c) {
+            case 'i':
+                file_name = optarg;
+                break;
+            case 'n':
+                host_name = optarg;
+                break;
+            case 'p':
+                port_number = optarg;
+                break;
+            case '?':
+                std::cerr << "Unknown option: " << optopt << ". Exiting" << std::endl;
+                exit(ERROR_UNKNOWN_PARAMETER);
+            case 'h':
+            default:
+                print_help();
+                exit(0);
+                break;
+        }
     }
+}
+
+void print_help() {
+    std::cout << "Usage: Transmitter [-i <filename>] [-n <hostname>] [-p <port number>] [-h]\n";
+    std::cout << "-i: Input file. Can take a path relative to the executable's location, or an absolute path\n";
+    std::cout << "-n: Hostname of the receiver. Can use IP address or DNS address.\n";
+    std::cout << "-p: Port number the receiver is listening on.\n";
+    std::cout << "-h: Display this help text";
 }
